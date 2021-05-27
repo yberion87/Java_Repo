e7627516e1a53c1c08fe9aec2e79c2bef740a00e
yberion87 <jbartek87@gmail.com>
2021-05-27 20:59:36 +0200

    First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Module 6, Prepare for testing");
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUserName();
+//
+//        if (result.equals("theForumUser")){
+//            System.out.println("test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
+        Calculator calculator = new Calculator(2, 2);
+        int resultSum = calculator.sum();
+        int resultSub = calculator.subtract();
+
+        if(resultSum==4 && resultSub==0){
+            System.out.println("TEST OK");
+        }else System.out.println("Test error !");
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,2 +1,27 @@
-package com.kodilla.testing.calculator;public class Calculator {
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    public  int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int sum(){
+       return getA() + getB();
+    }
+
+    public int subtract(){
+        return getA() - getB();
+    }
 }
