9e8cd08b3af28721777f32743a374fba3faeb265
yberion87 <jbartek87@gmail.com>
2021-05-30 19:56:58 +0200

    JuniT and mockito with bookLibrary

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookLibraryTest.java
@@ -0,0 +1,49 @@
+package com.kodilla.testing.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class BookLibraryTest {
+
+    @Test
+    public void testListBooksWithConditionsReturnList() {
+        // Given
+        LibraryDataBase libraryDatabaseMock = mock(LibraryDataBase.class);
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
+        List<Book> resultListOfBooks = new ArrayList<Book>();
+        Book book1 = new Book("Secrets of Alamo", "John Smith", 2008);
+        Book book2 = new Book("Secretaries and Directors", "Dilbert Michigan", 2012);
+        Book book3 = new Book("Secret life of programmers", "Steve Wolkowitz", 2016);
+        Book book4 = new Book("Secrets of Java", "Ian Tenewitch", 2010);
+        resultListOfBooks.add(book1);
+        resultListOfBooks.add(book2);
+        resultListOfBooks.add(book3);
+        resultListOfBooks.add(book4);
+        when(libraryDatabaseMock.listBooksWithCondition("Secret"))
+                .thenReturn(resultListOfBooks);
+
+        // When
+        List<Book> theListOfBooks = libraryDatabaseMock.listBooksWithCondition("Secret");
+
+        // Then
+        assertEquals(4, theListOfBooks.size());
+    }
+
+    @Test
+    public void testListBooksWithConditionMoreThan20() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testListBooksWithConditionFragmentShorterThan3() {
+        assertTrue(false);
+    }
+
+}
